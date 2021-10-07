ca39228f00df152db32410ac063ef0f3ff3a588c
NguyenLeAuto <ldnguyenit@gmail.com>
2021-10-07 18:58:50 +0700

    testing demo

---
+++ src/test/java/demojava/pack/AppTest.java
@@ -0,0 +1,21 @@
+package demojava.pack;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        System.out.println("code selenium testing");
+    }
+}
